a7666fa5cbb8735e3549274083975a1149ee74bb
riskyhelen05 <riskyhelen1@gmail.com>
2026-07-04 14:44:10 +0700

    feat: setup auth and role management using spatie

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,12 @@ use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
+    use HasRoles;
     use HasFactory, Notifiable;
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,36 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
 
-Route::middleware('auth')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+
+    // ROLE BASED ACCESS
+    Route::middleware(['role:admin'])->group(function () {
+        Route::get('/admin', function () {
+            return "Admin Only";
+        });
+    });
+
+    Route::middleware(['role:staff'])->group(function () {
+        Route::get('/staff', function () {
+            return "Staff Only";
+        });
+    });
+
+    Route::middleware(['role:manager'])->group(function () {
+        Route::get('/manager', function () {
+            return "Manager Only";
+        });
+    });
+
+    // PROFILE
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
 });
 
 require __DIR__.'/auth.php';
