b89c8c802341b0f6428cdd63047bbcef1fcdafdc
riskyhelen05 <riskyhelen1@gmail.com>
2026-07-06 15:28:50 +0700

    refactor(product): update product model relationships

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -6,23 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-        protected $fillable = [
-            'kode_barang',
-            'name',
-            'category_id',
-            'stock',
-            'location',
-            'condition',
-            'image'
-        ];
+    protected $fillable = [
+        'kode_barang',
+        'name',
+        'category_id',
+        'stock',
+        'location',
+        'condition',
+        'image'
+    ];
 
+    /**
+     * Relasi ke kategori
+     */
     public function category()
     {
-    return $this->belongsTo(Category::class);
+        return $this->belongsTo(Category::class);
     }
 
-    public function borrowings()
+    /**
+     * Relasi ke detail peminjaman
+     */
+    public function borrowingDetails()
     {
-    return $this->hasMany(Borrowing::class);
+        return $this->hasMany(BorrowingDetail::class);
     }
 }
